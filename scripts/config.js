8177ce26f296c9ff497aae5ecf50c7773aa9c8dc
Clara_Hyouten <ortega.clara13@orange.fr>
2024-02-15 21:16:08 +0100

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette'];
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste.", "Merci pour le poisson."];
+
+// Comme ces variables ne sont pas dans un bloc de code, elles ont une portée globale.
+// Donc elles sont liées à script.js (/!\ de bien avoir mis config avant script dans le html)
---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu();
+
+
+// J'ai mis ce code en commentaire pour pouvoir le récupérer lorsque j'en aurai besoin :
+
+// let saisieTexte = document.getElementById("inputEcriture");
+// console.log(saisieTexte);
+// let boutonValidationSaisieTexte = document.getElementById("btnValiderMot");
+// console.log(boutonValidationSaisieTexte);
+
+// let proposition = document.querySelector(".zoneProposition");
+// console.log(proposition);
+// let zoneScore = document.querySelector(".zoneScore span");
+// console.log(zoneScore);
+
+// let boutonRadioChoix = document.querySelectorAll(".optionSource input");
+// console.log(boutonRadioChoix);
+
+// for (i=0 ; i < boutonRadioChoix.length; i++) {
+//     console.log(boutonRadioChoix[i]);
+// }
